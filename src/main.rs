ac9d06f190291181777688f90d3f9f3e33b46428
Chad H <52187532+Eonsim@users.noreply.github.com>
2024-09-18 22:18:26 +1200

    refactored VCF and GS methods

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,14 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rayon::prelude::*;
-use rust_htslib::bcf::{Read, Reader};
 use std::collections::BTreeSet;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
 use std::fs::File;
-use std::i32;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::io::{BufWriter, Write};
+use std::io::{BufWriter, Read, Write};
 use std::simd::prelude::*;
 use std::sync::mpsc;
 use std::time::Instant;
@@ -25,6 +23,173 @@ const DISCOVERY: i32 = 300;
 const VER_MAX_ERR: i32 = 3;
 const MIN_INF_MARKERS: i32 = 20;
 const MAX_MARKERS: usize = 1907;
+
+fn expand_i32_to_u8_pairs_lsb(input: i32) -> [u8; 16] {
+    let mut result = [0u8; 16];
+    // Iterate over the 16 pairs of bits in the i32
+    for i in 0..16 {
+        // Extract the two least significant bits for each u8
+        let pair_of_bits = ((input >> (2 * i)) & 0b11) as u8;
+        // Store the pair in the result array
+        result[i] = pair_of_bits;
+    }
+    result
+}
+
+#[inline]
+fn gtconv(gt: u8) -> (i8, i32) {
+    match gt {
+        0 => (-1, 1),
+        1 => (0, 1),
+        2 => (1, 1),
+        _ => (0, 0),
+    }
+}
+
+#[inline]
+fn htsconv(gt: &[i32]) -> (i8, i32) {
+    match gt {
+        [2, 2] => return (-1i8, 1),
+        [2, 4] => return (0i8, 1),
+        [4, 4] => return (1i8, 1),
+        //"0|0" => return (-1i8, 1),
+        //"0|1" => return (0i8, 1),
+        //"1|0" => return (0i8, 1),
+        //"1|1" => return (1i8, 1),
+        _ => return (0i8, 0),
+    }
+}
+
+fn bytes_to_gts(profile_bytes: Vec<i8>) -> (Vec<i8>, i32) {
+    let mut snp_start = 0;
+    let mut end = snp_start + 4;
+    let mut snps = 0;
+    let mut gtp: Vec<i8> = Vec::with_capacity(1920);
+    let mut informative = 0;
+    while snp_start < 1920 {
+        let res0 = i32::from_le_bytes(profile_bytes[0..4].try_into().expect("nope"));
+        snp_start += 4;
+        end += 4;
+        let res1 = i32::from_le_bytes(profile_bytes[4..8].try_into().expect("nope"));
+        snp_start += 4;
+        end += 4;
+        let res2 = i32::from_le_bytes(profile_bytes[8..12].try_into().expect("nope"));
+        snp_start += 4;
+        end += 4;
+        let res3 = i32::from_le_bytes(profile_bytes[12..16].try_into().expect("nope"));
+        snp_start += 4;
+        end += 4;
+        let gts0 = expand_i32_to_u8_pairs_lsb(res0)
+            .into_iter()
+            .map(|x| gtconv(x))
+            .collect::<Vec<(i8, i32)>>();
+        let gts1 = expand_i32_to_u8_pairs_lsb(res1)
+            .into_iter()
+            .map(|x| gtconv(x))
+            .collect::<Vec<(i8, i32)>>();
+        let gts2 = expand_i32_to_u8_pairs_lsb(res2)
+            .into_iter()
+            .map(|x| gtconv(x))
+            .collect::<Vec<(i8, i32)>>();
+        let gts3 = expand_i32_to_u8_pairs_lsb(res3)
+            .into_iter()
+            .map(|x| gtconv(x))
+            .collect::<Vec<(i8, i32)>>();
+
+        for i in 0..16 {
+            gtp.push(gts0[i].0);
+            gtp.push(gts1[i].0);
+            gtp.push(gts2[i].0);
+            gtp.push(gts3[i].0);
+            snps += 4;
+            informative += gts0[i].1;
+            informative += gts1[i].1;
+            informative += gts2[i].1;
+            informative += gts3[i].1;
+        }
+    }
+    (gtp, informative)
+}
+
+fn read_gs(gsfile: String) -> (HashMap<i32, usize>, Vec<Vec<i8>>, Vec<i32>) {
+    let mut fbin = File::open(gsfile).expect("can't read");
+    let mut buffer = [0u8; 4];
+    let mut meta: Vec<usize> = vec![0; 4];
+
+    /* Load the metadata [Animal Num, Markers, Packed Markers, Blocks] */
+    for i in 0..meta.len() {
+        fbin.read(&mut buffer).expect("Can't write file");
+        meta[i] = i32::from_le_bytes(buffer) as usize;
+    }
+
+    let mut anml_idx = vec![0; meta[0]];
+
+    let mut anml_lookup: HashMap<i32, usize> = HashMap::with_capacity(meta[0]);
+    /* Store the animal ids by index */
+    for i in 0..meta[0] {
+        fbin.read(&mut buffer).expect("Can't write file");
+        anml_idx[i] = i32::from_le_bytes(buffer);
+        anml_lookup.insert(anml_idx[i], i);
+    }
+
+    /* Read the genotypes, convert and store */
+    let mut mygts: Vec<Vec<i8>> = vec![Vec::with_capacity(meta[2]); meta[0]];
+    let mut inform: Vec<i32> = vec![0; meta[0]];
+    let mut profile_buffer = [0u8; 480];
+
+    for an in 0..meta[0] {
+        fbin.read(&mut profile_buffer).expect("Can't write file");
+        let anpro = bytes_to_gts(profile_buffer);
+        mygts[an] = anpro.0;
+        inform[an] = anpro.1;
+    }
+
+    (anml_lookup, mygts, inform)
+}
+
+fn read_vcf(vcffile: String) -> (HashMap<i32, usize>, Vec<Vec<i8>>, Vec<i32>) {
+    use rust_htslib::bcf::{Read, Reader};
+    let mut bcf = Reader::from_path(vcffile).expect("Error opening file.");
+    let _ = bcf.set_threads(6);
+    let sample_count = usize::try_from(bcf.header().sample_count()).unwrap();
+    let mut anml_lookup: HashMap<i32, usize> = HashMap::with_capacity(sample_count);
+    let mut genotypes: Vec<Vec<i8>> = vec![Vec::with_capacity(MAX_MARKERS); sample_count];
+    let mut inform: Vec<i32> = vec![0; sample_count];
+    eprintln!("Indexing animals");
+    let vcf_samples: Vec<&str> = bcf
+        .header()
+        .samples()
+        .into_iter()
+        .map(|x| std::str::from_utf8(x).expect("err"))
+        .collect();
+    for sample_idx in 0..sample_count {
+        anml_lookup.insert(
+            vcf_samples[sample_idx]
+                .parse::<i32>()
+                .expect("conversion error"),
+            sample_idx,
+        );
+    }
+    eprintln!("Starting GT load");
+
+    for record in bcf.records().map(|r| r.expect("No record")) {
+        let gts: Vec<(i8, i32)> = record
+            .format(b"GT")
+            .integer()
+            .expect("GTs error")
+            .to_vec()
+            .iter()
+            .map(|a| htsconv(a))
+            .collect();
+        for (mygt, sample_index) in gts.iter().zip(0..sample_count) {
+            inform[sample_index] += mygt.1;
+            genotypes[sample_index].push(mygt.0);
+        }
+    }
+
+    eprintln!("Read VCF");
+    (anml_lookup, genotypes, inform)
+}
 
 fn vec_pars(child: &[i8], parent: &[i8], max_err: &i32) -> (i32, i32, i32, f64) {
     let mut start: usize = 0;
@@ -137,26 +302,12 @@ fn findparents(
     matches
 }
 
-#[inline]
-fn gtconv(gt: &[i32]) -> (i8, i32) {
-    match gt {
-        [2, 2] => return (-1i8, 1),
-        [2, 4] => return (0i8, 1),
-        [4, 4] => return (1i8, 1),
-        //"0|0" => return (-1i8, 1),
-        //"0|1" => return (0i8, 1),
-        //"1|0" => return (0i8, 1),
-        //"1|1" => return (1i8, 1),
-        _ => return (0i8, 0),
-    }
-}
-
 fn main() {
     let startt: Instant = Instant::now();
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 || args.contains(&String::from("-h")) {
         eprintln!(
-            "Error, requires 3 files:\n myprog my.vcf.gz my.ids.txt my.ped threadsN\nExiting."
+            "Error, requires 3 files:\n myprog my.vcf.gz my.ids.txt my.ped threadsN GS\nExiting."
         );
         use std::process;
         process::exit(0);
@@ -168,49 +319,22 @@ fn main() {
         &8
     };
 
-    let vcf: &str = &args[1];
+    let gsmthd: bool = if args.len() == 6 { true } else { false };
+
+    let gtfile: &str = &args[1];
     let anmls_file: &String = &args[2];
     let ped_file: &String = &args[3];
-    eprintln!("Loading VCF");
-    let mut bcf = Reader::from_path(vcf).expect("Error opening file.");
-    let _ = bcf.set_threads(6);
-    let sample_count = usize::try_from(bcf.header().sample_count()).unwrap();
-    let mut anml_lookup: HashMap<i32, usize> = HashMap::with_capacity(sample_count);
-    let mut genotypes: Vec<Vec<i8>> = vec![Vec::with_capacity(MAX_MARKERS); sample_count];
-    let mut inform: Vec<i32> = vec![0; sample_count];
-    eprintln!("Indexing animals");
-    let vcf_samples: Vec<&str> = bcf
-        .header()
-        .samples()
-        .into_iter()
-        .map(|x| std::str::from_utf8(x).expect("err"))
-        .collect();
-    for sample_idx in 0..sample_count {
-        anml_lookup.insert(
-            vcf_samples[sample_idx]
-                .parse::<i32>()
-                .expect("conversion error"),
-            sample_idx,
-        );
-    }
-    eprintln!("Starting GT load");
 
-    for record in bcf.records().map(|r| r.expect("No record")) {
-        let gts: Vec<(i8, i32)> = record
-            .format(b"GT")
-            .integer()
-            .expect("GTs error")
-            .to_vec()
-            .iter()
-            .map(|a| gtconv(a))
-            .collect();
-        for (mygt, sample_index) in gts.iter().zip(0..sample_count) {
-            inform[sample_index] += mygt.1;
-            genotypes[sample_index].push(mygt.0);
-        }
-    }
+    let myreader: fn(String) -> (HashMap<i32, usize>, Vec<Vec<i8>>, Vec<i32>) = if gsmthd {
+        println!("GS file enabled");
+        read_gs
+    } else {
+        println!("Reading VCF file");
+        read_vcf
+    };
 
-    eprintln!("Read VCF");
+    let (anml_lookup, genotypes, inform) = myreader(gtfile.to_string());
+
     let tfile: File = File::open(&anmls_file).expect("Failed to read animal file");
     let mut anmls_list: Vec<i32> = Vec::with_capacity(anml_lookup.len());
     let areader: BufReader<File> = BufReader::new(tfile);
